e299e542bae6b681375d75f1bf4ffac0223e689d
ajbeaver25 <81400612+ajbeaver25@users.noreply.github.com>
2025-04-09 17:54:26 -0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -201,7 +201,7 @@ pcall(function()
 					Gun("Remington 870")
 					new = LocalPlayer.Backpack:FindFirstChild("Remington 870")
 				end
-				for i = 1, 225 do
+				for i = 1, 250 do
 					Rstorage.ShootEvent:FireServer(america, new)
 				end
 				task.wait(1)
