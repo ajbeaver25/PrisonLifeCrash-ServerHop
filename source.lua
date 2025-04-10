eeb32ec6f45bd72c6e7928759bab2a1ae13a58f6
ajbeaver25 <81400612+ajbeaver25@users.noreply.github.com>
2025-04-09 19:45:16 -0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -392,7 +392,7 @@ pcall(function()
 	end)
 	
 	print("Crashing Server...")	
-	if #game.Players:GetPlayers() >= 5 then
+	if #game.Players:GetPlayers() >= 8 then
 		game:GetService("GuiService").ErrorMessageChanged:Wait()
 		warn("KICKED FROM GAME")
 		task.wait(1)
